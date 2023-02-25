12a9a0dac893f65f6461199fe5a46b64ea6b9c3b
mutasemjab <70386495+mutasemjab@users.noreply.github.com>
2023-02-25 16:29:11 +0300

    uuu

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>تسجيل الدخول</title>
+  <title>تسجيل الدخولvbgn</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
