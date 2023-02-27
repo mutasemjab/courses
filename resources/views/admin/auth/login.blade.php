a793863315137e4957d210a9c8984a9f6ab1a540
mutasem <mutasemms198@gmail.com>
2023-02-27 15:17:33 +0300

    hi

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>تسجيل الدخولvbgn</title>
+  <title>تسجيل الدخول</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -37,7 +37,7 @@
               <span class="fas fa-envelope"></span>
             </div>
           </div>
-        
+
         </div>
         @error('username')
         <span class="text-danger">{{ $message }}</span>
@@ -50,13 +50,13 @@
               <span class="fas fa-lock"></span>
             </div>
           </div>
-        
+
         </div>
         @error('password')
         <span class="text-danger">{{ $message }}</span>
       @enderror
         <div class="row">
-         
+
           <!-- /.col -->
           <div class="col-12">
             <button type="submit" class="btn btn-primary btn-block btn-flat">دخول </button>
@@ -65,7 +65,7 @@
         </div>
       </form>
 
-    
+
     </div>
     <!-- /.login-card-body -->
   </div>
